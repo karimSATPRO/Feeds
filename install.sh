88b884aa5d8dba494a1045972d75adfd77f9d526
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2023-05-24 15:13:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
-# Feeds
+#!/bin/sh
+echo "##############################################"
+echo "############ FEEDS[Emil-nabil] ##############"
+echo "############  v1.0[24-05-2023]  ##############"
+echo "##############################################"
+wget -O /tmp/igma2-plugin-extensions-feed-emil-nabil-v1.0_emil-nabil_all.ipk "https://github.com/karimSATPRO/Feeds/blob/main/enigma2-plugin-extensions-feed-emil-nabil-v1.0_emil-nabil_all.ipk"
+opkg install  --force-overwrite /tmp/*.ipk
+echo ""
+cd ..
+sync
+echo "##############################################"
+echo "############ Installation Successful  ########"
+echo "############   Restart Enigma2 GUI... ########" 
+echo "##############################################"
+init 4
+sleep 2
+init 3
+exit 0
